17d7520b2de6b5e96f38f7833c779900a2b39c1d
Rob Pando <robbpando@gmail.com>
2022-08-26 17:07:02 -0500

    Add contract minting restriction to handle mint function

---
+++ src/contracts/Minter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IndelibleContract {
+    function mint(uint256 count) external payable returns (uint);
+}
+
+contract TestMinterContract {
+  address public contractToMint;
+
+  function executeExternalContractMint(address toMint) external payable returns (bool) {
+    if (toMint == address(0)) {
+      return false;
+    }
+
+    IndelibleContract collectionContract = IndelibleContract(toMint);
+    uint tokens = collectionContract.mint{value: msg.value}(1);
+    if (tokens >= 0) {
+      return true;
+    }
+    return false;
+  }
+}
